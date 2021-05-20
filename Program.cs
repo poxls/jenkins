6064a34186e905f9b202f0769932a932965c56b3
poxls <61625277+poxls@users.noreply.github.com>
2021-05-20 17:01:23 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text.RegularExpressions;
+
+namespace jenkins
+{
+    class Program
+    {
+         static void Main(string[] args)
+        {
+            Console.Write("Введите свое имя: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Привет, {name}");
+        }
+    }
+}
